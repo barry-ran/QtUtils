4e576ce4683914691d5b06f839fa98229f9db5c8
rankun <rankun@bytedance.com>
2019-09-05 13:25:44 +0800

    fix: mac build

--- examples/FrameLessWindow/main.cpp
+++ examples/FrameLessWindow/main.cpp
@@ -1,18 +1,21 @@
 #include "widget.h"
 #include <QApplication>
 
+#ifdef Q_OS_WIN
 #include "framelesswindowhelper.h"
+#endif
 
 int main(int argc, char *argv[])
 {
     QApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
-    QApplication a(argc, argv);    
+    QApplication a(argc, argv);
 
+#ifdef Q_OS_WIN
     FramelessWindowHelper<Widget> w;
-    //Widget w;
-    //w.setResizeable(true);
-    //w.setTitleHeight(300);
     w.setResizeable(false);
+#else
+    Widget w;
+#endif
     w.show();
 
     return a.exec();
